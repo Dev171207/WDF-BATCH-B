39ad009711b2affcd76b1fb0da4474189d4aafee
Dev Pandya <devpandya6777@gmail.com>
2026-09-23 14:57:32 +0530

    Add files via upload

---
+++ PRACTICAL 4/HallLogin.js
@@ -0,0 +1,13 @@
+function showPassword()
+{
+    var password = document.getElementById("password");
+
+    if(password.type == "password")
+    {
+        password.type = "text";
+    }
+    else
+    {
+        password.type = "password";
+    }
+}
